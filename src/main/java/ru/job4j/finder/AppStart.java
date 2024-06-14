6bd0d5db1cdc0c4e84d066845acc6e0b42a093e2
Denis Trunov <smkslnc@gmail.com>
2024-06-14 17:29:25 +0500

    Update project 14.06.2024

--- src/main/java/ru/job4j/finder/AppStart.java
+++ src/main/java/ru/job4j/finder/AppStart.java
@@ -2,17 +2,18 @@ package ru.job4j.finder;
 
 import ru.job4j.finder.search.Search;
 
-import java.io.IOException;
-
 /**
  * Console application for searching files by name, mask or regex
- * @author Denis Trunov
- * @Date: 04.06.2024
+ * @author Denis Trunov - 04.06.2024
  */
 public class AppStart {
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Search search = new Search();
-        search.execute(args);
+        try {
+            search.execute(args);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
